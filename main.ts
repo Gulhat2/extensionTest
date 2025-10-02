d75cf71e682b11432ae6351c844109bf5b175d2e
Gulhat2 <gulhat2.0@gmail.com>
2025-10-02 23:20:11 +0200

    Opdater pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-function TestFunction () {
-    serial.writeString("Hej")
+//% color="#AA278D" weight=100 icon="\uf1ad" block="Basic Blocks"
+namespace hello {
+    //% block="Test Block"
+    export function helloWorld() {
+        serial.writeLine("Hello");
+        led.toggle(0, 0);
+    }
 }
